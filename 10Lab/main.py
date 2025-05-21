1f1d60b0f4a4adcf2eb902953b6d11620ddcd656
Jakub Pastuszka <jaku6p@gmail.com>
2025-05-21 18:43:23 +0200

    test

--- 10Lab/main.py
+++ 10Lab/main.py
@@ -19,43 +19,99 @@ def isPrime_Basic(n):
     return True
 
 
-def processLeader(queueLeader, queueResponse, eventStop, processNumber):
-    prmieMax = 0
+def processLeader(queueLeader, queueResponse, eventStop, processNumber=3):
+    primeMax = 0
 
-    # Program działa do momentu CTRL+C
-    while eventStop.is_set():
-
-        # Czekanie na wiadomość
+    while not eventStop.is_set():
         try:
             msg = queueLeader.get(timeout=1)
         except:
             continue
         
-        # Follower znalazł liczbę pierwszą
-        if msg[0] == "propsal":
+        if msg[0] == "proposal":
             _, idFrom, primeProposed = msg
 
-        # Sprawdzenie czy nowa liczba pierwsza jest największa
-        if primeProposed > prmieMax:
-             print(f"[Leader] Received proposal {primeProposed} from Follower {idFrom}")
+            if primeProposed > primeMax:
+                print(f"[Leader] Received proposal {primeProposed} from Follower {idFrom}")
 
-        # Wysłanie oraz czekanie na odpowiedzi od Followerów
-        votes = 1
-        for q in queueResponse:
-            q.put(("voteRequest", primeProposed))
+                votes = 1
+                for q in queueResponse:
+                    q.put(("voteRequest", primeProposed))
 
-        for q in queueResponse:
-            try:
-                respone = q.get(timeout=1)
-                if respone == "yes":
-                    vote += 1
-            except: 
-                continue
-        
+                for q in queueResponse:
+                    try:
+                        response = q.get(timeout=1)
+                        if response == "yes":
+                            votes += 1
+                    except:
+                        continue
 
-        if votes > (int(processNumber/2)+1):
-            prmieMax = primeProposed
-            print(f"[Leader] Prime {primeProposed} accepted by majority. Writing to file.")
+                if votes > (processNumber // 2):
+                    primeMax = primeProposed
+                    print(f"[Leader] Prime {primeProposed} accepted by majority. Writing to file.")
+                    with open("10Lab/primes.txt", "a") as f:
+                        f.write(f"{primeProposed} - by Follower {idFrom}\n")
 
-            with open("primes.txt", "a") as f:
-                f.write(str(primeProposed) + f" - by Follower {idFrom}\n")
+
+def processFollower(id, queueLeader, queueResponse, eventStop):
+    numberCurrent = 0
+    numberMaxFound = 0
+
+    while not eventStop.is_set():
+        if not queueResponse.empty():
+            msg = queueResponse.get()
+            if msg[0] == "voteRequest":
+                primeProposed = msg[1]
+                print(f"[Follower {id}] Voting YES for {primeProposed}")
+                queueResponse.put("yes")
+
+        if isPrime_Basic(numberCurrent):
+            if numberMaxFound < numberCurrent:
+                numberMaxFound = numberCurrent
+                queueLeader.put(("proposal", id, numberCurrent))
+                print(f"[Follower {id}] Found new prime: {numberCurrent}")
+                time.sleep(0.5)
+
+        numberCurrent += random.randint(1, 10)
+
+eventStop = multiprocessing.Event()
+
+def signalHandler(sig, frame):
+    print("\n[Main] Ctrl+C received. Stopping...")
+    eventStop.set()
+
+signal.signal(signal.SIGINT, signalHandler)
+
+
+if __name__ == "__main__":
+    manager = multiprocessing.Manager()
+    queueLeader = manager.Queue()
+    
+
+    # Odpowiedzi Followerów do Leadera
+    queueResponse1 = manager.Queue()
+    queueResponse2 = manager.Queue()
+
+    # Leader
+    leader = multiprocessing.Process(
+        target=processLeader,
+        args=(queueLeader, [queueResponse1, queueResponse2], eventStop)
+    )
+
+    follower1 = multiprocessing.Process(
+        target=processFollower,
+        args=(1, queueLeader, queueResponse1, eventStop)
+    )
+
+    follower2 = multiprocessing.Process(
+        target=processFollower,
+        args=(2, queueLeader, queueResponse2, eventStop)
+    )
+
+    leader.start()
+    follower1.start()
+    follower2.start()
+
+    leader.join()
+    follower1.join()
+    follower2.join()
